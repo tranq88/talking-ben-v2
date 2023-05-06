1a0c3c04f5ff873a8d1fcef19675b1508bf6e4a7
tranq88 <50079142+tranq88@users.noreply.github.com>
2023-05-06 18:35:47 -0400

    Update paginator.py

--- paginator.py
+++ paginator.py
@@ -4,7 +4,11 @@ import discord
 class Paginator:
     """Pagination with embeds."""
 
-    def __init__(self, elements: list[str], max_per_page):
+    def __init__(self,
+                 title: str,
+                 url: str,
+                 elements: list[str],
+                 max_per_page: int):
         """
         Split the strings in <elements> into pages,
         where each page has at most <max_per_page> strings.
@@ -19,12 +23,17 @@ class Paginator:
 
         self.pages = [
             discord.Embed(
-                title='Tags for Goldfish Gang',
+                title=title,
                 description=partition,
                 color=discord.Color.from_rgb(181, 142, 101)
             )
             for partition in partitions
         ]
+
+        for i, p in enumerate(self.pages):
+            p.set_thumbnail(url=url)
+            p.set_footer(text=f'Page {i+1} of {len(self)}')
+
         self.current_index = 0
 
     def __len__(self) -> int:
@@ -41,3 +50,45 @@ class Paginator:
 
     def goto_page(self, index: int):
         self.current_index = index
+
+
+class PaginatorButtons(discord.ui.View):
+    """Paginator controls."""
+
+    def __init__(self, paginator: Paginator):
+        super().__init__(timeout=60)
+        self.paginator = paginator
+
+    async def on_timeout(self):
+        self.clear_items()
+        await self.message.edit(view=self)
+
+    @discord.ui.button(label='Prev', disabled=True)
+    async def prev_page(self,
+                        interaction: discord.Interaction,
+                        button: discord.ui.Button):
+        self.paginator.prev_page()
+        self.next_page.disabled = False
+
+        if self.paginator.current_index == 0:
+            button.disabled = True
+
+        await interaction.response.edit_message(
+            embed=self.paginator.current_page(),
+            view=self
+        )
+
+    @discord.ui.button(label='Next')
+    async def next_page(self,
+                        interaction: discord.Interaction,
+                        button: discord.ui.Button):
+        self.paginator.next_page()
+        self.prev_page.disabled = False
+
+        if self.paginator.current_index == len(self.paginator) - 1:
+            button.disabled = True
+
+        await interaction.response.edit_message(
+            embed=self.paginator.current_page(),
+            view=self
+        )
