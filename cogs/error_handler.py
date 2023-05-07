baf284624c61b3819d7312f99285521ced36db70
tranq88 <50079142+tranq88@users.noreply.github.com>
2023-05-06 20:32:36 -0400

    Create error_handler.py

---
+++ cogs/error_handler.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+from env import BOT_TEST_SERVER, GFG_SERVER
+
+
+class ErrorHandler(commands.Cog):
+    """Handle errors."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self,
+                               ctx: commands.Context,
+                               error: commands.CommandError):
+        await ctx.reply(error)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(ErrorHandler(bot),
+                      guilds=[discord.Object(id=BOT_TEST_SERVER),
+                              discord.Object(id=GFG_SERVER)])
