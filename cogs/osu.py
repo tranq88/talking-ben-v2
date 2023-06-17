e2cd5e790ac50f6da6252af617a924bc65bbb807
tranq88 <50079142+tranq88@users.noreply.github.com>
2023-06-17 01:53:48 -0400

    move b!rs to its own function

--- cogs/osu.py
+++ cogs/osu.py
@@ -8,8 +8,7 @@ from jsons import read_json
 
 from utils.paginator import Paginator, reply_paginator
 from utils.account_registration import AccountRegistration, get_safe_name
-from utils.osu_utils import get_grade_emoji, calc_map_completion
-from utils.gfg_api import get_player_scores, get_player_info
+from utils.gfg_api import get_player_info
 from utils.gfg_server_accs import find_user
 from utils.emojis import (
     RANKING_SSH,
@@ -18,6 +17,7 @@ from utils.emojis import (
     RANKING_S,
     RANKING_A
 )
+from utils.osu_utils import get_recent_scores
 from requests.exceptions import HTTPError
 from ossapi import OssapiAsync
 
@@ -128,77 +128,16 @@ class Osu(commands.Cog):
     @commands.hybrid_command(
         name='recent',
         aliases=['rs'],
-        description=("Get a user's most recent play(s) on osu!Goldfish.")
+        description=(
+            "Get a user's most recent osu! Standard play(s) on osu!Goldfish."
+        )
     )
     @app_commands.guilds(BOT_TEST_SERVER, GFG_SERVER)
     async def recent(self,
                      ctx: commands.Context,
                      username: Optional[str] = None):
         await ctx.defer()
-
-        if not username:  # search for the user's account
-            server_accs = read_json('server_accs.json')
-            try:
-                username = find_user(server_accs, ctx.author.id)
-            except LookupError:
-                await ctx.reply('You are not registered on osu!Goldfish.')
-                return
-
-        try:
-            user = get_player_info(name=username)
-        except HTTPError:
-            await ctx.reply('User not found.')
-            return
-
-        user_scores = get_player_scores(name=user.name)
-        pages: list[discord.Embed] = []
-
-        for score in user_scores:
-            completion = (
-                f' ({calc_map_completion(score):.2f}%)' if score.grade == 'F'
-                else ''
-            )
-            em = discord.Embed(
-                description=(
-                    f"▸ {get_grade_emoji(score.grade)}{completion} ▸ "
-                    f"**{score.pp:.2f}pp** ▸ "
-                    # f"({calc_fc_pp()}PP for {calc_fc_acc()}% FC) ▸ "
-                    f"{score.acc:.2f}% ▸ "
-                    f"<t:{int(score.play_time.timestamp())}:R>"
-                    f"\n▸ {score.score:,} ▸ "
-                    f"x{score.max_combo}/{score.beatmap.max_combo} ▸ "
-                    f"[{score.n300}/{score.n100}/{score.n50}/{score.nmiss}]"
-                ),
-                timestamp=score.play_time,
-                colour=discord.Colour.from_rgb(181, 142, 101)
-            )
-
-            em.set_author(
-                name=(
-                    f'{score.beatmap.title} [{score.beatmap.difficulty}] '
-                    f'+{score.mods.short_name()} '
-                    f'[{score.beatmap.star_rating:.2f}★]'
-                ),
-                url=f'https://osu.ppy.sh/b/{score.beatmap.diff_id}',
-                icon_url=f'https://a.victoryu.dev/{user.id}'
-            )
-
-            em.set_thumbnail(
-                url=f'https://b.ppy.sh/thumb/{score.beatmap.set_id}l.jpg'
-            )
-
-            footer = (
-                "WYSI" if score.max_combo == 727 else "On osu!Goldfish server"
-            )
-            em.set_footer(text=footer)
-
-            pages.append(em)
-
-        await reply_paginator(
-            paginator=Paginator(pages=pages, show_index=False),
-            ctx=ctx,
-            content=f'**Recent osu! Standard Play for {user.name}:**'
-        )
+        await get_recent_scores(ctx=ctx, username=username, mode=0)
 
     @commands.hybrid_command(
         name='osu',
--- utils/osu_utils.py
+++ utils/osu_utils.py
@@ -1,5 +1,8 @@
 from typing import Optional
-from utils.gfg_api import Score
+
+from jsons import read_json
+from utils.gfg_api import Score, get_player_info, get_player_scores
+from utils.gfg_server_accs import find_user
 from utils.emojis import (
     RANKING_SSH,
     RANKING_SS,
@@ -11,6 +14,11 @@ from utils.emojis import (
     RANKING_D,
     RANKING_F
 )
+from utils.paginator import Paginator, reply_paginator
+
+import discord
+from discord.ext.commands import Context
+from requests.exceptions import HTTPError
 
 
 def get_grade_emoji(grade: str) -> Optional[str]:
@@ -38,3 +46,83 @@ def get_grade_emoji(grade: str) -> Optional[str]:
 def calc_map_completion(score: Score) -> float:
     """Calculate the percentage of map completed as given by <score>."""
     return score.time_elapsed / 1000 / score.beatmap.length * 100
+
+
+async def get_recent_scores(ctx: Context,
+                            username: Optional[str],
+                            mode: int) -> None:
+    """Get an osu!Goldfish user's most recent scores."""
+    if not username:  # search for the user's account
+        server_accs = read_json('server_accs.json')
+        try:
+            username = find_user(server_accs, ctx.author.id)
+        except LookupError:
+            await ctx.reply('You are not registered on osu!Goldfish.')
+            return
+
+    try:
+        user = get_player_info(name=username)
+    except HTTPError:
+        await ctx.reply('User not found.')
+        return
+
+    user_scores = get_player_scores(name=user.name, mode=mode)
+    pages: list[discord.Embed] = []
+
+    for score in user_scores:
+        completion = (
+            f' ({calc_map_completion(score):.2f}%)' if score.grade == 'F'
+            else ''
+        )
+        em = discord.Embed(
+            description=(
+                f"▸ {get_grade_emoji(score.grade)}{completion} ▸ "
+                f"**{score.pp:.2f}pp** ▸ "
+                # f"({calc_fc_pp()}PP for {calc_fc_acc()}% FC) ▸ "
+                f"{score.acc:.2f}% ▸ "
+                f"<t:{int(score.play_time.timestamp())}:R>"
+                f"\n▸ {score.score:,} ▸ "
+                f"x{score.max_combo}/{score.beatmap.max_combo} ▸ "
+                f"[{score.n300}/{score.n100}/{score.n50}/{score.nmiss}]"
+            ),
+            timestamp=score.play_time,
+            colour=discord.Colour.from_rgb(181, 142, 101)
+        )
+
+        em.set_author(
+            name=(
+                f'{score.beatmap.title} [{score.beatmap.difficulty}] '
+                f'+{score.mods.short_name()} '
+                f'[{score.beatmap.star_rating:.2f}★]'
+            ),
+            url=f'https://osu.ppy.sh/b/{score.beatmap.diff_id}',
+            icon_url=f'https://a.victoryu.dev/{user.id}'
+        )
+
+        em.set_thumbnail(
+            url=f'https://b.ppy.sh/thumb/{score.beatmap.set_id}l.jpg'
+        )
+
+        footer = (
+            "WYSI" if score.max_combo == 727 else "On osu!Goldfish server"
+        )
+        em.set_footer(text=footer)
+
+        pages.append(em)
+
+    modes = [
+        'osu! Standard',     # 0
+        'osu! Taiko',        # 1
+        'osu! Catch',        # 2
+        'osu! Mania',        # 3
+        'osu! Relax',        # 4
+        'osu! Relax Taiko',  # 5
+        'osu! Relax Catch',  # 6
+        None,                # placeholder
+        'osu! Autopilot'     # 8
+    ]
+    await reply_paginator(
+        paginator=Paginator(pages=pages, show_index=False),
+        ctx=ctx,
+        content=f'**Recent {modes[mode]} Play for {user.name}:**'
+    )
